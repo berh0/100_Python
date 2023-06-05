92ad2d2dc3a9281a616acde1966d7d42669a3690
berh0 <berham10@gmail.com>
2023-06-05 14:06:24 +0300

    auction_last

--- Day9/main.py
+++ Day9/main.py
@@ -1,10 +1,40 @@
-# logo = author: @natsuwumi
-# 2021/04/19 11:00
+# Project: Blind Auction
 
-# Day 9 Project: Secret Auction
+#from replit import clear
 
-from replit import clear
+# clear function without replit module
+import os
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 from art import logo
 
 print(logo)
 print("Welcome to the secret auction program.")
+
+bids = {}
+bidding_finished = False    # Flag
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    # bidding_record = {"Angela": 123, "James": 321}
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+while not bidding_finished:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+    if should_continue == "no":
+        bidding_finished = True
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        clear()
+
+
