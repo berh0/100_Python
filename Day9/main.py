55ceafa33e9a12efb88b6dd0b79206d1e46583f3
berh0 <berham10@gmail.com>
2023-06-05 13:58:14 +0300

    secret_auction

---
+++ Day9/main.py
@@ -0,0 +1,10 @@
+# logo = author: @natsuwumi
+# 2021/04/19 11:00
+
+# Day 9 Project: Secret Auction
+
+from replit import clear
+from art import logo
+
+print(logo)
+print("Welcome to the secret auction program.")
