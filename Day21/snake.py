dea7391f408ae476b7c2b0909ffe16fb8a7b7141
berh0 <berham10@gmail.com>
2023-06-14 14:38:08 +0300

    updated snake

--- Day21/snake.py
+++ Day21/snake.py
@@ -1,4 +1,4 @@
-from turtle import Turtle,Screen
+from turtle import Turtle
 STARTING_POSITION = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
@@ -9,7 +9,6 @@ RIGHT = 0
 class Snake():
 
     def __init__(self):
-        self.screen = Screen()
         self.segments = []
         self.create_snake()
         self.head = self.segments[0]
@@ -27,6 +26,13 @@ class Snake():
 
     def extend(self):
         self.add_segment(self.segments[-1].position())
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
