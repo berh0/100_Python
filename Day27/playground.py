3b5684a263578936a3b6c037e541e4501725d39a
berh0 <berham10@gmail.com>
2023-06-17 15:50:47 +0300

    *args,**kwargs playground

---
+++ Day27/playground.py
@@ -0,0 +1,37 @@
+# *args: Positional Variable-Length Arguments
+def add(*args):
+    # print(args[1])
+
+    sum = 0
+    for n in args:
+        sum += n
+    return sum
+# print(add(3, 5, 6, 2, 1, 7, 4, 3))
+
+
+# **kwargs: Keyworded Variable-Length Arguments
+def calculate(n, **kwargs):
+    print(kwargs)
+    # for key, value in kwargs.items():
+    #     print(key)
+    #     print(value)
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    # print(n)
+
+
+calculate(2, add=3, multiply=5)
+
+
+# How to use a **kwargs dictionary safely
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.color = kw.get("color")
+        self.seats = kw.get("seats")
+# get method returns none if the key doesn't exist
+# self.make = kw["make"] will throw an error if the key doesn't exist
+
+my_car = Car(make="Nissan", model="GT-R")
+print(my_car.model)
