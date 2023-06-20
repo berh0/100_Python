f386b1bd1b4baeea0e4ad392726b4ee17b4aa03c
berh0 <berham10@gmail.com>
2023-06-20 14:40:34 +0300

    Updated with exception handling

--- Day26/NATO-alphabet-start/main.py
+++ Day26/NATO-alphabet-start/main.py
@@ -5,12 +5,15 @@ data = pd.read_csv("nato_phonetic_alphabet.csv")
 # Keyword Method with iterrows()
 phonetic_dict = {row.letter:row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
-
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
-
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-word = input("Enter a word: ").upper()
+   
+while True:
+    try:
+        word = input("Enter a word: ").upper()
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(word)
+        break
 
 output_list = [phonetic_dict[letter] for letter in word]
 print(output_list)
