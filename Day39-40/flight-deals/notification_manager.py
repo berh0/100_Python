18405712f2501603d542107dc13d604534d1b584
berh0 <berham10@gmail.com>
2023-06-29 14:22:34 +0300

    notification manager

---
+++ Day39-40/flight-deals/notification_manager.py
@@ -0,0 +1,20 @@
+from smtplib import SMTP
+import os
+
+class NotificationManager:
+    
+    def __init__(self):
+        self.my_email = os.environ.get("MY_EMAIL")
+        self.password = os.environ.get("PASSWORD")
+
+    def send_email(self,message, google_flight_link):
+        
+        with SMTP("smtp.gmail.com") as self.smtp:
+            self.smtp = SMTP("smtp.gmail.com")
+            self.smtp.starttls()
+            self.smtp.login(user=self.my_email, password=self.password)    
+            self.smtp.sendmail(
+                from_addr=self.my_email,
+                to_addrs="TO_EMAIL",
+                msg=f"Subject:New Low Price Flight!\n\n{message}\n{google_flight_link}".encode("utf8")
+            )  
