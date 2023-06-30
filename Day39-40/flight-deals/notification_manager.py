6cee04d9351118d8b6540464054f0fd89a9c6273
berh0 <berham10@gmail.com>
2023-06-30 14:18:59 +0300

    Updated notification_manager.py

--- Day39-40/flight-deals/notification_manager.py
+++ Day39-40/flight-deals/notification_manager.py
@@ -7,14 +7,15 @@ class NotificationManager:
         self.my_email = os.environ.get("MY_EMAIL")
         self.password = os.environ.get("PASSWORD")
 
-    def send_email(self,message, google_flight_link):
+    def send_email(self,emails,message, google_flight_link):
         
-        with SMTP("smtp.gmail.com") as self.smtp:
-            self.smtp = SMTP("smtp.gmail.com")
-            self.smtp.starttls()
-            self.smtp.login(user=self.my_email, password=self.password)    
-            self.smtp.sendmail(
-                from_addr=self.my_email,
-                to_addrs="TO_EMAIL",
-                msg=f"Subject:New Low Price Flight!\n\n{message}\n{google_flight_link}".encode("utf8")
-            )  
+        for email in emails:
+            with SMTP("smtp.gmail.com") as self.smtp:
+                self.smtp = SMTP("smtp.gmail.com")
+                self.smtp.starttls()
+                self.smtp.login(user=self.my_email, password=self.password)    
+                self.smtp.sendmail(
+                    from_addr=self.my_email,
+                    to_addrs=email,
+                    msg=f"Subject:New Low Price Flight!\n\n{message}\n{google_flight_link}".encode("utf8")
+                )  
